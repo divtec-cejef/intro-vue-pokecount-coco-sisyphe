f508aa33fddc143c6dfd052cd6914048e9f55ab9
colinecharmillot <coline.charmillot@divtec.ch>
2024-09-20 21:24:58 +0200

    Progrès actuel - étapes 1,2,3 terminées - avancement de l'étape 4

--- script.js
+++ script.js
@@ -4,4 +4,27 @@
  * @date 2024-09-17
  */
 
-console.log('Intro à vue.js');
+const { createApp, ref } = Vue;
+
+createApp({
+    setup() {
+        const message = "Bienvenue dans PokeCount!";
+        const compteur = ref(0);
+        const capturesTab = ref([]);
+
+        function capturer() {
+            compteur.value += 1;
+        }
+
+        function sauver() {
+            if (compteur.value > 0) {
+                capturesTab.value.push(compteur.value);
+                compteur.value = 0;
+            }
+        }
+        console.log(compteur.value);
+        return { message };
+    }
+}).mount("#poke-app");
+
+
