bd7fa5132a260252c85a8bc563558558c27db4bb
colinecharmillot <coline.charmillot@divtec.ch>
2024-09-20 21:30:37 +0200

    Etat de base

--- script.js
+++ script.js
@@ -4,27 +4,6 @@
  * @date 2024-09-17
  */
 
-const { createApp, ref } = Vue;
-
-createApp({
-    setup() {
-        const message = "Bienvenue dans PokeCount!";
-        const compteur = ref(0);
-        const capturesTab = ref([]);
-
-        function capturer() {
-            compteur.value += 1;
-        }
-
-        function sauver() {
-            if (compteur.value > 0) {
-                capturesTab.value.push(compteur.value);
-                compteur.value = 0;
-            }
-        }
-        console.log(compteur.value);
-        return { message };
-    }
-}).mount("#poke-app");
+console.log(compteur.value);
 
 
